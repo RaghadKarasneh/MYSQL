9760c49c5e3e57d7650dfdf56721a787434ab561
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-05-16 15:54:38 +0300

    Tasks

--- Monday 16_05/index.php
+++ Monday 16_05/index.php
@@ -9,15 +9,28 @@ try {
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-
+//Insrt Records
 $sql = "INSERT INTO livecode (id,name, job_title) VALUES ('10','Marwa','Coffee girl')";
+//Delete Records
 $sql2="DELETE FROM livecode WHERE id='7'";
+//Update Records
 $sql3="UPDATE livecode SET name = 'Aya' WHERE id='6'";
+//Create Table
 $sql4="CREATE TABLE new_table (
     id INT(14) NOT NULL,
     user_name VARCHAR(255) NOT NULL,
     salary FLOAT(14,10) NOT NULL
 );";
-$conn->exec($sql4);
+//Join Statment
+$sql5 = "INSERT INTO new_table (id,user_name,salary) VALUES ('1','samar','700');";
+$sql6="SELECT * FROM livecode
+INNER JOIN new_table ON livecode.id=new_table.id;";
+//Execute Statment
+// $conn->exec($sql6);
+$users=$conn->query($sql6);
+
+foreach ($users as $row) {
+    print $row["user_name"] . " - " . $row["id"] . "<br/>";
+}
 
 ?>
